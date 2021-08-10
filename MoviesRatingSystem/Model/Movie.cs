33da7607660f68107de05b9c6ee2d7d42fbe9acb
Shalom Malovicki Asus <shalomm2468@gmail.com>
2021-08-10 15:46:12 +0300

    add class Movie

---
+++ MoviesRatingSystem/Model/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesRatingSystem.Model
+{
+    public class Movie
+    {
+        private long movieId;
+        private string movieDescription;
+        private int totalVotes;
+        private DateTime lastUpdated;
+
+        public long MovieId { get => movieId; set => movieId = value; }
+        public string MovieDescription { get => movieDescription; set => movieDescription = value; }
+        public int TotalVotes { get => totalVotes; set => totalVotes = value; }
+        public DateTime LastUpdated { get => lastUpdated; set => lastUpdated = value; }
+    }
+}
