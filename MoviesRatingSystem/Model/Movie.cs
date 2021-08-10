8ef2d46cf9acd32ff6d8f7a87d1b0ade30358862
Shalom Malovicki Asus <shalomm2468@gmail.com>
2021-08-10 15:59:06 +0300

    implement OnePropertyChanged in movie

--- MoviesRatingSystem/Model/Movie.cs
+++ MoviesRatingSystem/Model/Movie.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MoviesRatingSystem.Resources;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,16 +7,16 @@ using System.Threading.Tasks;
 
 namespace MoviesRatingSystem.Model
 {
-    public class Movie
+    public class Movie : OnePropertyChanged
     {
         private long movieId;
         private string movieDescription;
         private int totalVotes;
         private DateTime lastUpdated;
 
-        public long MovieId { get => movieId; set => movieId = value; }
-        public string MovieDescription { get => movieDescription; set => movieDescription = value; }
-        public int TotalVotes { get => totalVotes; set => totalVotes = value; }
-        public DateTime LastUpdated { get => lastUpdated; set => lastUpdated = value; }
+        public long MovieId { get => movieId; set => SetField(ref movieId , value); }
+        public string MovieDescription { get => movieDescription; set => SetField(ref movieDescription , value); }
+        public int TotalVotes { get => totalVotes; set => SetField(ref totalVotes, value); }
+        public DateTime LastUpdated { get => lastUpdated; set => SetField(ref lastUpdated , value); }
     }
 }
