5f34a5eb9292cc13c8601f4da037ebed69e054b6
Shalom Malovicki Asus <shalomm2468@gmail.com>
2021-08-11 18:38:08 +0300

    add class MainViewModel

---
+++ MoviesRatingSystem/ViewModel/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using MoviesRatingSystem.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesRatingSystem.ViewModel
+{
+    class MainViewModel : OnePropertyChanged
+    {
+        #region Private Fields
+        private DateTime lastReceive;
+        #endregion Private Fields
+
+        #region Ctor       
+        public MainViewModel()
+        {
+        }
+        #endregion Ctor
+
+        #region Properties
+        public DateTime LastReceive { get => lastReceive; set => SetField(ref lastReceive, value); }
+
+        #endregion Properties
+
+        #region Function
+        #endregion Function
+    }
+}
