0991e885aa194b1124566fbd74c435b7bd5767f1
Shalom Malovicki Asus <shalomm2468@gmail.com>
2021-08-11 17:42:13 +0300

    add new project MoviesRatingSystem_Test + fill class Program

---
+++ MoviesRatingSystem_Test/Program.cs
@@ -0,0 +1,31 @@
+﻿using MoviesRatingSystem.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesRatingSystem_Test
+{
+    class Program
+    {
+        static void Main()
+        {
+            var api = new MovieRestApi();
+
+            RestTests(api).Wait();
+            Console.ReadLine();
+        }
+
+        private static async Task RestTests(MovieRestApi api)
+        {         
+            var dateStart = DateTime.UtcNow.AddHours(-24);
+            var dateEnd = DateTime.UtcNow;
+         
+            var r1 = api.GetMoviesDescrption().Result;
+         
+            Console.WriteLine($"\n\n{r1}");
+        }
+
+    }
+}
