ecaa2864efdb5b5bd6bd7cdcab892fa735df6487
Shalom Malovicki Asus <shalomm2468@gmail.com>
2021-08-11 20:03:04 +0300

    add MoviesCollection + set MainViewModel

---
+++ MoviesRatingSystem/Model/MoviesCollection.cs
@@ -0,0 +1,33 @@
+﻿using MoviesRatingSystem.Resources;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesRatingSystem.Model
+{
+    public class MoviesCollection : OnePropertyChanged
+    {
+        #region Private Fields
+        private ObservableCollection<Movie> list;
+        private int mostVotes;
+        #endregion Private Fields
+
+        #region Ctor
+        public MoviesCollection() 
+        {
+            list = new ObservableCollection<Movie>();
+        }
+        #endregion Ctor
+
+        #region Properties
+        public ObservableCollection<Movie> List { get => list; set => SetField(ref list , value); }
+        public int MostVotes { get => mostVotes; set => SetField(ref mostVotes , value); }      
+        #endregion Properties
+
+        #region Function
+        #endregion Function
+    }
+}
--- MoviesRatingSystem/ViewModel/MainViewModel.cs
+++ MoviesRatingSystem/ViewModel/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using MoviesRatingSystem.Resources;
+﻿using MoviesRatingSystem.Model;
+using MoviesRatingSystem.Resources;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,18 +11,22 @@ namespace MoviesRatingSystem.ViewModel
     class MainViewModel : OnePropertyChanged
     {
         #region Private Fields
+        private bool serverStatus;
         private DateTime lastReceive;
+        private MoviesCollection moviesCollection;
         #endregion Private Fields
 
         #region Ctor       
         public MainViewModel()
         {
+            moviesCollection = new MoviesCollection();
         }
         #endregion Ctor
 
-        #region Properties
+        #region Properties     
         public DateTime LastReceive { get => lastReceive; set => SetField(ref lastReceive, value); }
-
+        public bool ServerStatus { get => serverStatus; set => SetField(ref serverStatus  , value); }
+        public MoviesCollection MoviesCollection { get => moviesCollection; set => SetField(ref moviesCollection  , value); }
         #endregion Properties
 
         #region Function
