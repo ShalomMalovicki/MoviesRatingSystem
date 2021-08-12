0d5476ba3e818993690ad7c72a6de85c56c69720
Shalom Malovicki Asus <shalomm2468@gmail.com>
2021-08-12 05:52:52 +0300

    set & use in BooleanToStringConverter

--- MoviesRatingSystem/Converter/BooleanToBrushConverter.cs
+++ MoviesRatingSystem/Converter/BooleanToBrushConverter.cs
@@ -20,9 +20,31 @@ namespace MoviesRatingSystem.Converter
             bool.TryParse(value.ToString(), out isTrue);
 
             if (isTrue)
-                return Brushes.Green;
+                return Brushes.Green;//"Disconnect"
             else
                 return Brushes.Red;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class BooleanToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return Brushes.Transparent;
+
+            bool isTrue;
+            bool.TryParse(value.ToString(), out isTrue);
+
+            if (isTrue)
+                return "Disconnect";
+            else
+                return "Connect";
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
