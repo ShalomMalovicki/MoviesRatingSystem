893871ff3e32997855da6684438d149bfdcbcd86
Shalom Malovicki Asus <shalomm2468@gmail.com>
2021-08-12 14:07:40 +0300

    add notes part 1

--- MoviesRatingSystem/Converter/BooleanToBrushConverter.cs
+++ MoviesRatingSystem/Converter/BooleanToBrushConverter.cs
@@ -9,28 +9,7 @@ using System.Windows.Media;
 
 namespace MoviesRatingSystem.Converter
 {
-    public class BooleanToBrushConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (value == null)
-                return Brushes.Transparent;
-
-            bool isTrue;
-            bool.TryParse(value.ToString(), out isTrue);
-
-            if (isTrue)
-                return Brushes.Green;//"Disconnect"
-            else
-                return Brushes.Red;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
+    // use for Connect Button Content
     public class BooleanToStringConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
@@ -45,54 +24,6 @@ namespace MoviesRatingSystem.Converter
                 return "Disconnect";
             else
                 return "Connect";
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
-    public class IntegerToBrushConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (value == null)
-                return Brushes.Transparent;
-
-            int mostVotes = (int)parameter;  
-            bool isTrue = (int)value == mostVotes;
-         
-            if (isTrue)
-                return Brushes.Yellow;
-            else
-                return Brushes.Transparent;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
-    public class IntegerToStringConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            return value.ToString();
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
-    public class StringToIntegerConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {           
-            return int.Parse(value as string);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
